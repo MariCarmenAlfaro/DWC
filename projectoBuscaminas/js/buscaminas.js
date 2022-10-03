80303f468a8d317ab82e485b9ec9e2cc2e76d1d2
MªCarmenAlfaro <maricarmenalfaro1616@gmail.com>
2022-10-03 17:53:34 +0200

    clase dia 03/10

--- projectoBuscaminas/js/buscaminas.js
+++ projectoBuscaminas/js/buscaminas.js
@@ -1,9 +1,33 @@
-document.write('<table>')
-document.write('<tr>')
+let maxFilas=prompt('¿Cuantas filas quieres?');
+let maxColumnas=prompt('¿Cuantas colunas quieres?');
+let numMinas= prompt('¿Qué numero de minas deseas?')
 
-for (let i =0; i< 0; i++){
-    document.write('<td></td>')
+
+//Creamos el tablero en HTML
+document.write('<table>')
+
+
+for (let i = 0; i < maxFilas; i++) {
+    document.write('<tr>')
+
+    for (let i =0; i< maxColumnas; i++){
+        document.write('<td></td>')
+    }
+    
+    document.write('<tr>')
+    
 }
 
-document.write('<tr>')
 document.write('<table>')
+
+
+//Array bidimensional para guardar las minas
+
+let arrayTablero=[];
+for (let mina = 0; mina < numMinas; mina++) {
+    posFila= Math.floor(Math.random()*maxFilas);
+    console.log(posFila)
+   // arrayTablero[posFila][posColumna]='MINA';
+    
+}
+
