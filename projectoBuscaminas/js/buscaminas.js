4b1075ed3dcf31a83d8ad6f8e1fccb03c23f1187
MªCarmenAlfaro <maricarmenalfaro1616@gmail.com>
2022-10-04 19:12:25 +0200

    Clase 04/10, seguimiento projecto Buscaminas

--- projectoBuscaminas/js/buscaminas.js
+++ projectoBuscaminas/js/buscaminas.js
@@ -1,8 +1,8 @@
-let maxFilas=prompt('¿Cuantas filas quieres?');
-let maxColumnas=prompt('¿Cuantas colunas quieres?');
-let numMinas= prompt('¿Qué numero de minas deseas?')
+let maxFilas = prompt('¿Cuantas filas quieres?');
+let maxColumnas = prompt('¿Cuantas colunas quieres?');
+let numMinas = prompt('¿Qué numero de minas deseas?')
 
-
+/*
 //Creamos el tablero en HTML
 document.write('<table>')
 
@@ -19,15 +19,33 @@ for (let i = 0; i < maxFilas; i++) {
 }
 
 document.write('<table>')
-
+*/
 
 //Array bidimensional para guardar las minas
 
-let arrayTablero=[];
-for (let mina = 0; mina < numMinas; mina++) {
-    posFila= Math.floor(Math.random()*maxFilas);
-    console.log(posFila)
-   // arrayTablero[posFila][posColumna]='MINA';
+let arrayTablero = [];
+let contadorMinas = 0;
+let posFila;
+let posColumna;
+
+for (let fila = 0; fila < maxFilas; fila++) {
+    arrayTablero[fila] = [];
+
+   for (let columna = 0; columna <maxColumnas; columna++) {
+        arrayTablero[fila][columna]='';
     
+   }
 }
+while (contadorMinas < numMinas) {
+    posFila = Math.floor(Math.random() * maxFilas);
+    posColumna = Math.floor(Math.random() * maxColumnas);
+
+    if (arrayTablero[posFila][posColumna] != 'MINA') {
+        arrayTablero[posFila][posColumna] = 'MINA';
+        contadorMinas++;
+    }
+};
+
+console.log(arrayTablero);
+console.log(contadorMinas);
 
