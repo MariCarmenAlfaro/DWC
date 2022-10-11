0fef257e04fb7c5e5393fa34408acbc0df179efb
MariCarmenAlfaro <101742672+MariCarmenAlfaro@users.noreply.github.com>
2022-10-11 12:28:13 +0200

    debugeo codigo

--- projectoBuscaminas/js/buscaminas.js
+++ projectoBuscaminas/js/buscaminas.js
@@ -36,6 +36,7 @@ for (let fila = 0; fila < maxFilas; fila++) {
 
     }
 }
+//sacamos aleatoriamente las posiciones
 while (contadorMinas < numMinas) {
     posFila = Math.floor(Math.random() * maxFilas);
     posColumna = Math.floor(Math.random() * maxColumnas);
@@ -46,24 +47,35 @@ while (contadorMinas < numMinas) {
     }
 };
 
+//Miramos de asignar los numeros alrededor de las posiciones de las minas
 let fila = 2;
 let columna = 3;
 let numMinasAlrededor = 0;
-
+debugger
+//recorremos fila y coluna par ver si hay minas
 for (let fila = 0; fila < maxFilas; fila++) {
-    for (let cFila = fila - 1; cFila > fila + 1; cFila++) {
-        for (let cColumna = columna - 1; cColumna > columna + 1; cColumna++) {
-            if (cFila >= 0 && cFila < maxFilas && cColumna >= 0 &&cColumna<maxColumnas) {
-                if(arrayTablero[cFila][cColumna] == 'MINA') {
-                    numMinasAlrededor++;
+    for (let columna = 0; columna < maxColumnas; columna++) {
+        numMinasAlrededor = 0;
+        //si NO hay minas entonces 
+        if (arrayTablero[fila][columna] != 'MINA'){
+            //la posiciones de la fila y la columna empieza 1- hasta 1+, osea al rededor de cada posicion
+            for (let cFila = fila - 1; cFila < fila + 1; cFila++) {
+                for (let cColumna = columna - 1; cColumna < columna + 1; cColumna++) {
+                    //pues si dicha posicion es mayor= 0 y es menor que su maximo(que hemos indicado al principio), si se cumple todo eso entonces
+                    if ((cFila >= 0 && cFila < maxFilas) && (cColumna >= 0 && cColumna < maxColumnas)) {
+                        //pon en esa posicion una mina
+                        if (arrayTablero[cFila][cColumna] == 'MINA') {
+                            //añade al contador que ahi hay una mina
+                            numMinasAlrededor++;
+                        }
+                    } 
                 }
             }
-
+            arrayTablero[fila][columna] = numMinasAlrededor;
         }
-    }
-
-}
-
+    
 
 arrayTablero[fila][columna] = numMinasAlrededor;
 console.log(arrayTablero);
+    }
+}
