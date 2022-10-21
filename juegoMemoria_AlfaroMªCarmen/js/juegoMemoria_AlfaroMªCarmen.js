a1990a956ea4eb2605629f82985db7d0d1028705
MariCarmenAlfaro <101742672+MariCarmenAlfaro@users.noreply.github.com>
2022-10-21 13:43:29 +0200

    avance del programa, agregar objeto, posicionar imagenes.

--- juegoMemoria_AlfaroMªCarmen/js/juegoMemoria_AlfaroMªCarmen.js
+++ juegoMemoria_AlfaroMªCarmen/js/juegoMemoria_AlfaroMªCarmen.js
@@ -1,28 +1,41 @@
 class Tablero {
-     filasUsuario = "";
-     columnasUsuario ="";
+    filasUsuario = "";
+    columnasUsuario = "";
     constructor() {
 
         this.saberColumnasyFilas();
-        this.crearTablero();5
-    
+        this.calcularSiEsPar();
+        this.crearTablero();
+
         this.pintarTablero();
     }
 
+
+
     saberColumnasyFilas() {
-        debugger
-        this.filasUsuario = prompt('¿Cuántas filas quieres?');
-        this.columnasUsuario = prompt('¿Cuántas columnas quieres?');
-        
+
+        this.filasUsuario = parseInt(prompt('¿Cuántas filas quieres?'));
+        this.columnasUsuario = parseInt(prompt('¿Cuántas columnas quieres?'));
+
     }
-   
+
     //const tableroBuscaminas = new Tablero(3,5);
     // console.log(tableroBuscaminas);
-   
+    calcularSiEsPar() {
+
+        let impar = true;
+        while (impar) {
+            if ((this.filasUsuario * this.columnasUsuario) % 2 == 0) {
+                impar = false;
+            } else {
+                alert("El tablero debe ser par.")
+                this.saberColumnasyFilas();
+            }
+        }
+
+    }
 
     crearTablero() {
-        console.log(this.filasUsuario)
-        console.log(this.columnasUsuario)
 
         this.arrayTablero = [];
 
@@ -35,6 +48,7 @@ class Tablero {
         }
     }
     pintarTablero() {
+
         document.write('<table>');
 
         for (let i = 0; i < this.filasUsuario; i++) {
@@ -49,15 +63,58 @@ class Tablero {
         document.write('</table>');
     }
 }
-/*class JuegoMemoria extends Tablero() {
-    constructor(filas, columnas) {
-        this.filas = filas;
-        this.columnas = columnas;
 
-        this.saberColumnasyFilas();
-        this.crearTablero();
-        this.pintarTablero();
+class JuegoMemoria extends Tablero {
+    constructor() {
+        super();
+        this.colocarImagenes();
+        this.pintarImgs();
     }
-}*/
+    colocarImagenes() {
+        let imagenesSeleccionadasAnimales = 0;
+        let tamanyoTablero = this.filasUsuario * this.columnasUsuario;
+        let arrayImagenes = ['imgs/caballo.jpg', 'imgs/cerdo.jpg', 'imgs/elefante.jpg', 'imgs/erizo.jpg', 'imgs/gato.jpg', 'imgs/leon.jpg', 'imgs/panda.jpg', 'imgs/perro.jpeg', 'imgs/pollito.jpg', 'imgs/vaca.jpg'];
 
-const tableroNuevo = new Tablero();
+
+        let arrayImgsAleatorias = [];
+        debugger
+        while  (arrayImgsAleatorias.length < tamanyoTablero) {
+         arrayImgsAleatorias.push(arrayImagenes[imagenesSeleccionadasAnimales]);
+         arrayImgsAleatorias.push(arrayImagenes[imagenesSeleccionadasAnimales]);
+            imagenesSeleccionadasAnimales++
+            if (arrayImagenes.length == imagenesSeleccionadasAnimales) {
+                imagenesSeleccionadasAnimales = 0
+            }
+
+            console.log (arrayImgsAleatorias.length);
+        }
+        // console.log arrayImgsAleatorias)
+
+        for (let i = 0; i < tamanyoTablero; i++) {
+         arrayImgsAleatorias = arrayImgsAleatorias.sort(function () { return Math.random() - 0.5 });
+
+        }
+
+        console.log (arrayImgsAleatorias);
+
+
+    }
+    pintarImgs() {
+        for (let i = 0; i <this.filasUsuario; i++) {
+            for (let j = 0; j < this.columnasUsuario; j++) {
+                
+                this.arrayTablero= this.arrayImgsAleatorias[i][j];
+            }
+          
+            
+        }
+        console.log(this.arrayTablero);
+       
+     
+    }
+}
+
+const tableroNuevo = new JuegoMemoria();
+
+
+
