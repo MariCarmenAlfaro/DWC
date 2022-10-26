19dba7b67a33a22da7ae8cada492a01dc3ce256f
MªCarmenAlfaro <maricarmenalfaro1616@gmail.com>
2022-10-26 19:10:18 +0200

    Funciona todo correctamente

--- juegoMemoria_AlfaroMªCarmen/js/juegoMemoria_AlfaroMªCarmen.js
+++ juegoMemoria_AlfaroMªCarmen/js/juegoMemoria_AlfaroMªCarmen.js
@@ -1,26 +1,23 @@
-
+//Creamos un objeto Tablero para definir la estructura como tal.
 class Tablero {
     //Definimos variables globales para usar en todo el código.
     filasUsuario = "";
     columnasUsuario = "";
     constructor() {
-
         this.saberColumnasyFilas();
         this.calcularSiEsPar();
         this.crearTablero();
-
     }
 
+     //Método para preguntar al usuario las filas y columnas que desee para formar el tablero
     saberColumnasyFilas() {
         alert("¡VAMOS A JUGAR!");
-        //Método para preguntar al usuario las filas y columnas que desee para formar el tablero
         this.filasUsuario = parseInt(prompt('¿Cuántas filas quieres?'));
         this.columnasUsuario = parseInt(prompt('¿Cuántas columnas quieres?'));
-
     }
 
+    //Con éste método comprobamos si los valores que nos ha dado dan un tablero par, si no le avisamos de que no lo es y volvemos a preguntar.
     calcularSiEsPar() {
-        //Con éstemétodo comprobamos si los valores que nos ha dado dan un tablero par, si no le avisamos de que no lo es y volvemos a preguntar.
         let impar = true;
         while (impar) {
             if ((this.filasUsuario * this.columnasUsuario) % 2 == 0) {
@@ -32,9 +29,9 @@ class Tablero {
         }
 
     }
-    crearTablero() {
 
-        //Creamos el tablero
+    //Creamos el tablero
+    crearTablero() {
         this.arrayTablero = [];
 
         for (let fila = 0; fila < this.filasUsuario; fila++) {
@@ -45,11 +42,9 @@ class Tablero {
         }
     }
 
+    //Mostramos el tablero ya formado
     pintarTablero() {
-
-        //Mostramos el tablero ya formado
         document.write('<table>');
-
         for (let i = 0; i < this.filasUsuario; i++) {
             document.write('<tr>');
             for (let j = 0; j < this.columnasUsuario; j++) {
@@ -57,12 +52,11 @@ class Tablero {
                 document.write(`<td> <img class="imagen" src="${this.arrayTablero[i][j]}" > </td>`);
             }
             document.write('</tr>');
-
         } document.write('</table>');
     }
 }
 
-
+//Creamos el objeto JuegoMemoria para formar todos los valores necesarios y meterlos en nuestro Tablero.
 class JuegoMemoria extends Tablero {
     //Variable global para poder usar en varios métodos
     arrayImgsAleatorias = [];
@@ -70,21 +64,20 @@ class JuegoMemoria extends Tablero {
         super();
         this.colocarImagenes();
         this.pintarTablero();
-
     }
+
     //Método para recibir las imágenes, crear su pareja, y dar una posición desordenada pero sin que se repita.
     colocarImagenes() {
-        
         let imagenesSeleccionadasAnimales = 0;
         let tamanyoTablero = this.filasUsuario * this.columnasUsuario;
         let arrayImagenes = ['imgs/caballo.jpg', 'imgs/cerdo.jpg', 'imgs/elefante.jpg', 'imgs/erizo.jpg', 'imgs/gato.jpg', 'imgs/leon.jpg', 'imgs/panda.jpg', 'imgs/perro.jpeg', 'imgs/pollito.jpg', 'imgs/vaca.jpg'];
         let casilla = 0;
-        //Bucle para desordenar todas las imagenes y que así nunca se escojan las mismas.
+        //Bucle para desordenar todas las imagenes y que así nunca se escojan de primeras las mismas.
         for (let i = 0; i < tamanyoTablero; i++) {
             arrayImagenes = arrayImagenes.sort(function () { return Math.random() - 0.5 });
 
         }
-        //Con el bucle creamos un nuevo array con las parejas necesarias para el tamaño definido de tablero
+        //Con el bucle, creamos un nuevo array metiendo parejas necesarias para el tamaño definido de tablero
         while (this.arrayImgsAleatorias.length < tamanyoTablero) {
             this.arrayImgsAleatorias.push(arrayImagenes[imagenesSeleccionadasAnimales]);
             this.arrayImgsAleatorias.push(arrayImagenes[imagenesSeleccionadasAnimales]);
@@ -93,24 +86,22 @@ class JuegoMemoria extends Tablero {
                 imagenesSeleccionadasAnimales = 0;
             }
         }
-        //Desordenamos el array para que salgan en diferentes posiciones.
+        //Desordenamos el array para que se coloquen en diferentes posiciones dentro del tablero.
         for (let i = 0; i < tamanyoTablero; i++) {
             this.arrayImgsAleatorias = this.arrayImgsAleatorias.sort(function () { return Math.random() - 0.5 });
 
         }
 
-        //Metemos el array de las imagenes tal y como queremos y lo añadimos a la tabla para sacar por pantalla.
-
+        //Metemos el array de la manera deseada, en parejas de forma aleatorias, y lo añadimos al tablero para sacar por pantalla.
         for (let i = 0; i < this.filasUsuario; i++) {
             for (let j = 0; j < this.columnasUsuario; j++) {
-
                 this.arrayTablero[i][j] = this.arrayImgsAleatorias[casilla];
                 casilla++;
             }
         }
     }
 }
-//Creamos el objeto
+//Declaramos el objeto para su utilización.
 let tableroNuevo = new JuegoMemoria();
 
 
