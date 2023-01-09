48dfbd8bb7982383ce80530601c885f518c577c1
MªCarmenAlfaro <maricarmenalfaro1616@gmail.com>
2023-01-09 16:50:51 +0100

    Creacion carpeta version 2 del Juego de Memoria.

--- Exercici1/exercici1_clase.js
+++ Exercici1/exercici1_clase.js
@@ -1,22 +1,28 @@
-document.addEventListener("DOMContentLoaded", function (event) {
+window.onload = function () {
   // debugger
-  let objeto_a = document.getElementsByTagName("a");
-  let objeto_p = document.getElementsByTagName("p");
-    let contador=0;
-    for (let i = 0; i < objeto_a.length; i++) {
-      if(objeto_a[i].href=="http://prueba" ||objeto_a[i].href=="http://prueba/"){
-        contador++;
-      }
-    };
-document.getElementById("info").innerHTML=
-    "Numero de enlaces que hay en la página: " +
-    objeto_a.length +
-    "<br>Numero de parrafos que hay en la página: " +
-    objeto_p.length+
-    "<br>El penultimo enlace apunta a :"+objeto_a[objeto_a.length-2]+
-    "<br>Último enlace apunta a :"+objeto_a[objeto_a.length-1]+
-  
-    "<br>"+contador+" enlaces apuntan a: http://prueba";
-    
-    
-});
+  let nodoDiv = document.getElementById('info');
+  let nodoLink = document.getElementsByTagName("a");
+  let nodoP1 = document.createElement("p");
+  let contador = 0;
+  let contador1 = 0;
+  for (let i = 0; i < nodoLink.length; i++) {
+    if (nodoLink[i].href == "http://prueba/") {
+      contador++;
+    }
+    if (nodoLink) {
+      contador1++;
+    }
+  }
+    nodoP1.innerHTML =
+      "1.Numero de enlaces que hay en la página: " +
+      nodoLink.length;
+
+    "<br>2.Numero de parrafos que hay en la página: " +
+      nodoP1.length +
+      "<br>3.El penultimo enlace apunta a :" + nodoLink[nodoLink.length - 2] +
+      "<br>4.Último enlace apunta a :" + nodoLink[nodoLink.length - 1] +
+
+      "<br>5. " + contador + " enlaces apuntan a: http://prueba";
+    "<br>6. Número total de enlaces " + contador1;
+
+  };
---
+++ JuegoMemoria_2_AlfaroMªCarmen/README.md
@@ -0,0 +1,10 @@
+# JUEGO DE MEMORIA
+¡Bienvenido/a al juego de memoria!
+
+Para comenzar a jugar deberás de introducir dos números para crear el tamaño del tablero que desees. 
+
+Tu juego funciona en todos los navegadores.
+
+¡RECUERDA! tu tablero debe ser par.
+
+A distrutar encontrando las parejas
